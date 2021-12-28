cc2324a402a7c76bde11a137e155698757bd2a81
raulsposito <raulsposito@gmail.com>
2021-12-28 13:07:35 -0300

    rm lax js

--- pages/index.vue
+++ pages/index.vue
@@ -25,24 +25,6 @@
         >
           Welcome to Vue Astro
         </h2>
-        <!-- <h2
-          class="
-            grow
-            fadeIn
-            text-4xl
-            tracking-tight
-            font-extrabold
-            text-white
-            sm:text-7xl
-          "
-        >
-          What do the stars have for you today?
-        </h2>
-        <p class="downarrows pt-32 text-lg font-semibold text-white">
-          scroll down
-        </p> -->
-        <!-- <p class="downarrows pt-12 text-xl font-semibold text-white">↓</p>
-        <p class="downarrows pt-12 text-xl font-semibold text-white">☆</p> -->
           <select
             id="sign"
             v-if="!this.isReading"
@@ -102,114 +84,9 @@
 </template>
 
 <script>
-// import lax from 'lax.js'
 import { mapState } from 'vuex'
 
 export default {
-  // mounted() {
-  //   lax.init()
-  //   lax.addDriver('scrollY', function () {
-  //     return window.scrollY
-  //   })
-  //   lax.addElements('.fadeIn', {
-  //     scrollY: {
-  //       opacity: [
-  //         [0, 100],
-  //         [0, 1],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.scrolldown', {
-  //     scrollY: {
-  //       'letter-spacing': [
-  //         [0, 'screenHeight*0.75'],
-  //         [0, 150],
-  //         {
-  //           cssUnit: 'px',
-  //         },
-  //       ],
-  //       opacity: [
-  //         [0, 100],
-  //         [1, 0],
-  //       ],
-  //       translateX: [
-  //         [0, 'screenHeight'],
-  //         [0, 80],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.fadeOut', {
-  //     scrollY: {
-  //       opacity: [
-  //         [0, 100],
-  //         [1, 0],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.letter-x', {
-  //     scrollY: {
-  //       translateY: [
-  //         [-400, 0],
-  //         [100, 0],
-  //       ],
-  //       translateX: [
-  //         [220, 'screenHeight'],
-  //         [0, 400],
-  //       ],
-  //       opacity: [
-  //         [0, 100],
-  //         [0, 1],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.letter-a', {
-  //     scrollY: {
-  //       translateY: [
-  //         [-400, 0],
-  //         [200, 0],
-  //       ],
-  //       translateX: [
-  //         [220, 'screenHeight'],
-  //         [0, -400],
-  //       ],
-  //       opacity: [
-  //         [0, 100],
-  //         [0, 1],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.downarrows', {
-  //     scrollY: {
-  //       translateY: [
-  //         [0, 75],
-  //         [0, 200],
-  //       ],
-  //       opacity: [
-  //         [0, 100],
-  //         [1, 0],
-  //       ],
-  //     },
-  //   })
-  //   lax.addElements('.grow', {
-  //     scrollY: {
-  //       scale: [
-  //         [30, 100],
-  //         [0.3, 1],
-  //       ],
-  //     },
-  //   })
-  // },
-  // beforeUnmount() {
-  //   lax.removeElements(
-  //     '.fadeIn',
-  //     '.grow',
-  //     '.downarrows',
-  //     '.letter-a',
-  //     '.letter-x',
-  //     '.fadeOut',
-  //     '.scrolldown'
-  //   )
-  // },
   data() {
     return {
       selected: '',
