d8c8d4825a423ee1070327bfccb99833cb6da550
raulsposito <raulsposito@gmail.com>
2022-01-18 22:23:16 -0300

    cm GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    { src: '~/plugins/vue-gtag', }
+    // { src: '/plugins/vue-gtag', }
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
