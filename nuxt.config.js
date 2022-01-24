3f325686cc2e8e5bafeea7cdce4bf3d6110cbed3
raulsposito <raulsposito@gmail.com>
2022-01-24 14:33:56 -0300

    add twitter card and vuegtag plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,15 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Get your horoscope for today!' },
       { name: 'format-detection', content: 'telephone=no' },
       { hid: 'og-type', property: 'og:type', content: 'website' },
       { hid: 'og-title', property: 'og:title', content: 'Vue Astro' },
       { hid: 'og-desc', property: 'og:description', content: 'Get your horoscope for today!' },
       { hid: 'og-image', property: 'og:image', content: 'https://media4.giphy.com/media/hXv4LngASTk8o/giphy.gif?cid=790b7611b5d57084fdba408839d3085fe40f0810b93ba17f&rid=giphy.gif&ct=g'},
       { hid: 'og-url', property: 'og:url', content: 'https://vue-astro.netlify.app/' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'https://media4.giphy.com/media/hXv4LngASTk8o/giphy.gif?cid=790b7611b5d57084fdba408839d3085fe40f0810b93ba17f&rid=giphy.gif&ct=g' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@raul_sposito' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -30,6 +32,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: './plugins/vue-gtag.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
